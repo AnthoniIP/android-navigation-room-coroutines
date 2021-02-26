0684098a065da87a2046316d1e0e7e87a4abb101
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-02-26 16:05:52 -0300

    First Commit

---
+++ app/src/main/java/com/ipsoft/android_navigation_room_coroutines/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ipsoft.android_navigation_room_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
